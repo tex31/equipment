013eab333af9ad722122214299000fd28ca1021e
nixiton <nyaina.andriambolamalala@gmail.com>
2017-10-30 11:02:21 +0100

    nix operation.java

--- src/main/java/com/douane/entite/Operation.java
+++ src/main/java/com/douane/entite/Operation.java
@@ -1,13 +1,22 @@
 package com.douane.entite;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.douane.model.EtatOperation;
 
 @Entity
 @Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
@@ -16,25 +25,44 @@ public abstract class Operation implements Serializable{
 	@Id
 	@GeneratedValue(strategy=GenerationType.TABLE)
 	protected Long id;
-	protected String date;
-	protected String poste;
 	
-	@transient
+	@Temporal(TemporalType.DATE)
+	protected Date date;
+	@Temporal(TemporalType.TIME)
+	protected Date time;
+	
+	protected String poste;
+	protected String motifretour;
+	
+	@ManyToOne
+	@JoinColumn(name="idOperateur")
 	protected Agent operateur;
+	
+	@Enumerated(EnumType.STRING)
+	protected EtatOperation state;
 	
 	public Long getId() {
 		return id;
 	}
+	
 
-
-
-	public String getDate() {
+	public Date getDate() {
 		return date;
 	}
 
 
-	public void setDate(String date) {
+	public void setDate(Date date) {
 		this.date = date;
+	}
+
+
+	public Date getTime() {
+		return time;
+	}
+
+
+	public void setTime(Date time) {
+		this.time = time;
 	}
 
 
@@ -48,13 +76,42 @@ public abstract class Operation implements Serializable{
 	}
 
 
-	public Agent getOperateur() {
+	public Operation(Date date, Date time, String poste, Agent operateur) {
+		super();
+		this.date = date;
+		this.time = time;
+		this.poste = poste;
+		this.operateur = operateur;
+		this.state = EtatOperation.WAITING;
+	}
+	public void valider() {
+		this.state = EtatOperation.ACCEPTED;
+	}
+	public void amodifier(String motif) {
+		this.motifretour = motif;
+		this.state = EtatOperation.MODIF;
+	}
+	public void arefuser(String motif) {
+		this.motifretour = motif;
+		this.state = EtatOperation.REFUSED;
+	}
+	
+
+
+	public Operation() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	/*public Agent getOperateur() {
 		return operateur;
 	}
 
 
 	public void setOperateur(Agent operateur) {
 		this.operateur = operateur;
-	}
+	}*/
+	
 
 }
